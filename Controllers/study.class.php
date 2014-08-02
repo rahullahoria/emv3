ac09a0ef31d6a6be7aacc792bae347919a53e07d
root <root@cap-lap-360.(none)>
2014-08-02 12:38:25 +0530

    2nd ayg

--- Controllers/study.class.php
+++ Controllers/study.class.php
@@ -45,17 +45,64 @@ class Study extends Controller {
 		echo "</p><p>";
 		$this->startSlideShow("1");
 		echo "</p>";
+		echo "<audio controls>
+    			<source src='http://socloc.capillary.in/rahulSandbox/outfile.mp3' type='audio/mpeg'>
+					Your browser does not support the audio element.
+				</audio>";
 
 	}
 
-	function startSlideShow($chapterID){
+	function nextSlide(){
+		require 'Models/Slide.class.php';
+
+		echo "nextSlide bellow";
+		$_SESSION['slide_no'] = $_SESSION['slide_no'] + 1;
+		$slideObj = new Slide($_SESSION['Chapter_id'],$_SESSION['slide_no']);
 
 		
+		
+
+		echo $slideObj->getHeading()."<br/>";
+		echo $slideObj->getPoints()."<br/>";
+		echo $slideObj->getNotes()."<br/>";
+		
+		echo "<a href='".URL."study/previousSlide/'>Previous</a>";
+		echo "<a href='".URL."study/nextSlide/'>Next</a><br/>";
+
+				
+	}
+
+	function previousSlide(){
 		require 'Models/Slide.class.php';
-		$slideObj = new Slide($chapterID);
-		echo $slideObj->getHeading();
-		echo $slideObj->getPoints();
-		echo $slideObj->getNotes();
+		
+		echo "Previous bellow: ".$_SESSION['slide_no']."<br/>";
+		if ($_SESSION['slide_no'] > 1)
+		$_SESSION['slide_no'] = $_SESSION['slide_no'] - 1 ;
+
+		$slideObj = new Slide($_SESSION['Chapter_id'],$_SESSION['slide_no']);
+
+		
+		echo $slideObj->getHeading()."<br/>";
+		echo $slideObj->getPoints()."<br/>";
+		echo $slideObj->getNotes()."<br/>";
+		echo "<a href='".URL."study/previousSlide/'>Previous</a>";
+		echo "<a href='".URL."study/nextSlide/'>Next</a><br/>";
+				
+	}
+
+	function startSlideShow($chapterID){
+		
+		require 'Models/Slide.class.php';
+		
+		$slideObj = new Slide($chapterID,"1");
+
+		$_SESSION['Chapter_id'] = $chapterID;
+		$_SESSION['slide_no'] = "1";
+
+		echo $slideObj->getHeading()."<br/>";
+		echo $slideObj->getPoints()."<br/>";
+		echo $slideObj->getNotes()."<br/>";
+		echo "<a href='".URL."study/nextSlide/'>Next</a><br/>";
 	}
 	function getSubjectIndex($subID){
 
